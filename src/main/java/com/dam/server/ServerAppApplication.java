0f7f5220eee55f25a8fcb3bf893ef3be342b6934
Raymond <yuhongweng0501@gmail.com>
2024-06-02 20:14:09 +0800

    Update ServerAppApplication.java

--- src/main/java/com/dam/server/ServerAppApplication.java
+++ src/main/java/com/dam/server/ServerAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerAppApplication extends Thread {
+public class ServerAppApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerAppApplication.class, args);
